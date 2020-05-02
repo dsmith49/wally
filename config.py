547a7a4d713b9ec5ea795abf681364b02a45cb9d
david <dsmith49@gmail.com>
2020-05-02 17:45:34 -0500

    none

---
+++ config.py
@@ -0,0 +1,13 @@
+
+speed = 200
+numlines = 8
+
+# constants to be set before use 
+x_total = 1.915
+motor1_length = 1.6
+motor2_length = 1.6
+x_usable = [0.70,1.22]
+y_usable = [0.30,1.00]
+button_delay = 0.05
+#distance_per_step = (2 * 0.025 * 3.14159) / 400 # = 0.00039269875 # meters per step # theoretical
+distance_per_step = (0.55 / 13300) # = 0.00004135338  in practice
