da5d49964fdcabf5621267dec5760b7685644b83
david <dsmith49@gmail.com>
2020-05-03 16:30:11 -0500

    none

--- wally.py
+++ wally.py
@@ -23,14 +23,14 @@ class Gondola(object):
 		self.motors_position = motors_position
 
 	def tocoord(self, loc ): #puts gondola pen at upper left of loc box
-		delta_x = (loc[0] - self.position[0]) * self. boxsize[0]
-		delta_y = (loc[1] - self.position[1]) * self.boxsize[1]
-		movecommand_x = [delta_x, -delta_x]
-		movecommand_y = [delta_y, delta_y]
+		delta_x = (loc[0] - self.position[0]) * config.boxsize[0] #self. boxsize[0]
+		delta_y = (loc[1] - self.position[1]) * config.boxsize[1] #self.boxsize[1]
+		#movecommand_x = [delta_x, -delta_x]
+		#movecommand_y = [delta_y, delta_y]
 		if (self.position[0] != loc[0]):
-			self.motors_position = motorlib.move( self.speed, movecommand_x, self.motors_position )
+			self.motors_position = motorlib.move( self.speed, [delta_x,0], self.motors_position )
 		if (self.position[1] != loc[1]):
-			self.motors_position = motorlib.move( self.speed, movecommand_y, self.motors_position )
+			self.motors_position = motorlib.move( self.speed, [0,delta_y], self.motors_position )
 		self.position = loc
 		
 	def togglepen(self):
@@ -46,20 +46,22 @@ class Gondola(object):
 		vertical_move = 0
 		for y in range(0,lines):
 			if (y == 0):
-				self.motors_position = motorlib.move( self.speed, [int(self.boxsize[1]/(lines)/2),int(self.boxsize[1]/(lines)/2)], self.motors_position )
-				vertical_move += int(self.boxsize[1]/(lines)/2)
+				#self.motors_position = motorlib.move( self.speed, [int(self.boxsize[1]/(lines)/2),int(self.boxsize[1]/(lines)/2)], self.motors_position )
+				self.motors_position = motorlib.move( self.speed, [0,int(config.boxsize[1]/(lines)/2)], self.motors_position )
+				vertical_move += int(config.boxsize[1]/(lines)/2)
 			else:
-				self.motors_position = motorlib.move( self.speed, [int(self.boxsize[1]/(lines)),int(self.boxsize[1]/(lines))], self.motors_position )
-				vertical_move += int(self.boxsize[1]/(lines))
+				#self.motors_position = motorlib.move( self.speed, [int(self.boxsize[1]/(lines)),int(self.boxsize[1]/(lines))], self.motors_position )
+				self.motors_position = motorlib.move( self.speed, [0,int(config.boxsize[1]/(lines))], self.motors_position )
+				vertical_move += int(config.boxsize[1]/(lines))
 			self.togglepen()
 			if (y % 2 == 0):
-				self.motors_position = motorlib.move( self.speed, [self.boxsize[0],-self.boxsize[0]], self.motors_position)
+				self.motors_position = motorlib.move( self.speed, [config.boxsize[0],0], self.motors_position)
 			else:
-				self.motors_position = motorlib.move( self.speed, [-self.boxsize[0],self.boxsize[0]], self.motors_position)
+				self.motors_position = motorlib.move( self.speed, [-config.boxsize[0],0], self.motors_position)
 			self.togglepen()
-		self.motors_position = motorlib.move( self.speed, [-vertical_move ,-vertical_move], self.motors_position )
+		self.motors_position = motorlib.move( self.speed, [0, -vertical_move , self.motors_position )
 		if (lines > 0 and lines % 2 != 0):
-			self.motors_position = motorlib.move( self.speed, [-self.boxsize[0],self.boxsize[0]], self.motors_position )
+			self.motors_position = motorlib.move( self.speed, [-config.boxsize[0],0], self.motors_position )
 
 def loadfile():
 	filename = sys.argv[1]
