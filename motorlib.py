baaed0c1aab35aca3515b448e7210f162aa49e88
david <dsmith49@gmail.com>
2020-05-07 16:18:49 -0500

    none

--- motorlib.py
+++ motorlib.py
@@ -160,7 +160,7 @@ def move_smart_step( speed, command, motors_position ):
 	motor1_velocity = motor_velocity_at_time( current_position, end_position, total_time/2, total_time )
 	motor2_velocity = motor_velocity_at_time( current_position_mirror, end_position_mirror, total_time/2, total_time )
 
-	#print('in smart step with moves', motors_position, motors_position_end, steps)
+	print('in smart step with moves', motors_position, motors_position_end, steps)
 	MOTOR.enablestepSTOPint(0,'A')          #set up to interrupt when motor a stops
 	MOTOR.enablestepSTOPint(0,'B') 
 	if (command[0] == 0 and command[1] == 0): return motors_position
