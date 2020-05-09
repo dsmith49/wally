ae80849fd9ffbb943914ed4b9ba6e6936ec879c0
david <dsmith49@gmail.com>
2020-05-09 09:19:42 -0500

    none

--- motorlib.py
+++ motorlib.py
@@ -76,7 +76,7 @@ def hypoteni_to_euclid( motors_position ):
 	
 	#height of trapezoid where a is long base, c is short base
 	y_from_top  = ((a+b-c+d) * (-a+b+c+d) * (a-b-c+d) * (a+b-c-d) / (4 * (a-c)**2))**0.5 + config.y_gondola
-	x_from_left = (motors_position[0]*config.meters_per_step)**2 - (y_from_top - config.y_gondola)**2 )**0.5
+	x_from_left = ((motors_position[0]*config.meters_per_step)**2 - (y_from_top - config.y_gondola)**2 )**0.5
 
 	return [x_from_left, y_from_top]
 	
