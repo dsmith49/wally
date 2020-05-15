1c98058deb1f470e273135930b96b8a03ffa27d5
david <dsmith49@gmail.com>
2020-05-15 17:38:36 -0500

    none

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,8 @@ from flask import Flask
 from wally_json_control import Wally
 
 app = Flask(__name__)
-#wally = Wally()
-#app.config.from_object( wally )
+wally = Wally()
+app.config.from_object( wally )
 
 from app import routes
 
