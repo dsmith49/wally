a65981045d56db30f476bd7a0e22a764c9ea5f69
david <dsmith49@gmail.com>
2020-05-15 14:19:31 -0500

    added new files

---
+++ wally_json_control.py
@@ -0,0 +1,97 @@
+import json
+import motorlib
+import config
+
+class Wally(Object):
+	def __init__(self):
+		self.motors_on = False
+		self.motors_velocity = [0,0]
+		self.motors_position = [int(config.motor1_length / config.meters_per_step), int(config.motor2_length / config.meters_per_step) ]
+		self.motors_last_velocity = [0,0]
+		self.timestamp_1 = 0.0
+		self.increment = 500
+		self.pwm = None #motorlib.configmotors( 0 )
+		self.pendown = False
+
+	def status(self):
+		statusdict = {
+			'power'    : self.motors_on,
+			'pendown'  : seldf.pendown,
+			'velocity' : self.motors_velocity,
+			'position_steps'    : self.motors_position,
+			'position_meters'   : [self.motors_position[0] * config.meters_per_step, self.motors_position[1] * config.meters_per_step],
+			'position_euclid'   : motorlib.hypoteni_to_euclid( self.motors_position )
+		}
+		return status_dict #json.dumps( statusdict )
+
+	def power(self, on=True):
+		if (on and not self.motors_on):
+			self.pwm = motorlib.configmotors( 0 )
+			self.motors_on = True
+		if (not on and self.motors_on):
+			motorlib.close( self.pwm )
+
+	def pause(self):
+		motorlib.stop()
+
+	def pen_move(self, down=None):
+		if (down is None):
+			self.pendown = not self.pendown
+		else:
+			sel.pendown = down
+		if (not self.pendown):
+			motorlib.setangle(self.pwm,0,config.pen_up_angle)
+		else:
+			motorlib.setangle(self.pwm,0,config.pen_down_angle)
+
+	def pen_rotate(self, position):
+		self.pen_move( down=False )
+		if (position==0): motorlib.setangle(self.pwm,1,config.pen_1_angle)
+		if (position==1): motorlib.setangle(self.pwm,1,config.pen_2_angle)
+		if (position==2): motorlib.setangle(self.pwm,1,config.pen_3_angle)
+
+	def calibrate(self):
+		self.motors_velocity = [0,0]
+		self.motors_position = [int(config.motor1_length / config.meters_per_step), int(config.motor2_length / config.meters_per_step) ]
+
+	def command(self, command_json):
+		self.motors_last_velocity = self.motors_velocity.copy()
+		command = command_json['command']
+		if (command == 'UP'):
+			self.motors_velocity[0] -= self.increment
+			self.motors_velocity[1] -= self.increment
+		elif (command == 'DOWN'):
+			self.motors_velocity[0] += self.increment
+			self.motors_velocity[1] += self.increment
+		elif (command == "STOP"):
+			self.motors_velocity[0] = 0
+			self.motors_velocity[1] = 0
+		elif (command == "UPLEFT"):
+			self.motors_velocity[0] -= self.increment
+		elif (command == "UPRIGHT"):
+			self.motors_velocity[1] -= self.increment
+		elif (command == "DOWNLEFT"):
+			self.motors_velocity[1] += self.increment
+		elif (command == "DOWNRIGHT"):
+			self.motors_velocity[0] += self.increment
+		elif (command == "LEFT"):
+			self.motors_velocity[0] -= self.increment
+			self.motors_velocity[1] += self.increment
+		elif (command == "RIGHT"):
+			self.motors_velocity[0] += self.increment
+			self.motors_velocity[1] -= self.increment
+		elif (command == "PEN1"): self.pen_rotate(0)
+		elif (command == "PEN2"): self.pen_rotate(1)
+		elif (command == "PEN3"): self.pen_rotate(2)
+		elif (command == "PENDOWN"): self.pen_move(down=True)
+		elif (command == "PENUP"): self.pen_move(down=False)
+		elif (command == "POWER"): self.power(on=not self.motors_on)
+		elif (command == "CALIBRATE"): self.calibrate()
+		elif (command == "MOVE"):
+			self.motors_velocity = [0,0]
+		self.motors_position = motorlib.update_motors( self.motors_last_velocity, self.motors_velocity, self.timestamp_1, self.motors_position)
+		if (command == "MOVE"):
+			self.motors_position = motorlib.move_smart2( command_json['speed'], command_json['relative_coords'], self.motors_position )
+		timestamp_1 = time.perf_counter()
+		time.sleep(config.button_delay)
+
---
+++ wally_terminal.py
@@ -0,0 +1,61 @@
+from wally_json_control import Wally
+import sys, termios, tty, os, time
+from subprocess import call
+
+def getch():
+	fd = sys.stdin.fileno()
+	old_settings = termios.tcgetattr(fd)
+	try:
+		tty.setraw(sys.stdin.fileno())
+		ch = sys.stdin.read(1)
+	finally:
+		termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+	return ch
+
+def display( status ):
+	call('clear')
+	print('DIRECTIONS: qwe        STOP: s')
+	print('            a d        PENDOWN:[      PENUP:]   COMMAND_NAIVE:=    COMMAND_SMART:-')
+	print('            zxc        EXIT: `        EXIT_AND_RELEASE_MOTORS:~')
+	print('PEN1: 1     PEN2: 2    PEN3: 3')
+	print('--------------------------------')
+	print('       motor1      |      motor2')
+	print('velocity: ',status['velocity'][0],'        ',status['velocity'][1])
+	print('steps: ',status['position_steps'][0],'  ',status['position_steps'][1])
+	print('length: ',"{:.2f}".format(status['position_meters'][0]),'  ',"{:.2f}".format(status['position_meters'][1]))
+	print('Euclid:',"{:.2f}".format(status['position_euclid'][0]),'  ',"{:.2f}".format(status['position_euclid'][1]))
+
+def control_repl():
+	exit = False
+	wally = Wally()
+	while not exit:
+		status = wally.status()
+		display( status )
+		char = getch()
+		command_dict = {}
+		if (char == "1"): command_dict['command'] = 'PEN1'
+		if (char == "2"): command_dict['command'] = 'PEN2'
+		if (char == "3"): command_dict['command'] = 'PEN3'
+		if (char == '['): command_dict['command'] = 'PENDOWN'
+		if (char == ']'): command_dict['command'] = 'PENUP'
+		if (char == "w"): command_dict['command'] = 'UP'
+		if (char == "x"): command_dict['command'] = 'DOWN'
+		if (char == "s"): command_dict['command'] = 'STOP'
+		if (char == "q"): command_dict['command'] = 'UPLEFT'
+		if (char == "e"): command_dict['command'] = 'UPRIGHT'
+		if (char == "z"): command_dict['command'] = 'DOWNLEFT'
+		if (char == "c"): command_dict['command'] = 'DOWNRIGHT'
+		if (char == "a"): command_dict['command'] = 'LEFT'
+		if (char == "d"): command_dict['command'] = 'RIGHT'
+		if (char == "`"): command_dict['command'] = 'POWER'
+		if (char == "~"): command_dict['command'] = 'PAUSE'
+		if (char == "p"): command_dict['command'] = 'CALIBRATE'
+		if (char == '-'):
+			command_dict['command'] = 'MOVE'
+			response = input('SMART MOVE >> SPEED X_meters Y_meters>>')
+			command_dict['speed'] = int(response.split(' ')[0])
+			command_dict['relative_coords'] = [ float(response.split(' ')[1]), float(response.split(' ')[2]) ]
+		wally.command( command_dict )
+
+if __name__ == '__main__':
+	control_repl()
