04998a4fadbb597c2c25410ec44e567a8bacd480
david <dsmith49@gmail.com>
2020-05-15 16:05:26 -0500

    none

--- wally_json_control.py
+++ wally_json_control.py
@@ -118,6 +118,7 @@ class Wally(object):
 	def command(self, command_json):
 		self.motors_last_velocity = self.motors_velocity.copy()
 		command = command_json['command']
+		print('command:',command)
 		if (not self.motors_on and command != 'POWER'): return None
 		if (command == 'UP'):
 			self.motors_velocity[0] -= self.increment
