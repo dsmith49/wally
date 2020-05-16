68484892d4c82419e6f1ad31527f10699554fd95
david <dsmith49@gmail.com>
2020-05-16 15:56:39 -0500

    none

--- app/static/draw.js
+++ app/static/draw.js
@@ -7,41 +7,12 @@ getupdate()
 document.getElementById("svg_list").onchange = function(){loadsvg()}
 
 function loadsvg() {
-	//var img = document.createElement('IMG')
-	console.log(document.getElementById("svg_image"))
 	var select = document.getElementById("svg_list")
-	console.log(select)
 	var filename = select.options[select.selectedIndex].value
-	var astring = "{{url_for('static', filename='images/" + filename + "')}}"
 	var bstring = 'static/images/' + filename
-	//img.src = '"' + astring + '"'
-	console.log( document.getElementById("svg_image") )
 	document.getElementById("svg_image").src = bstring
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	//document.getElementById("svg_container").removeChild( document.getElementById("svg_container").childNodes[0] )
 }
+
 function getupdate() {
 	$.ajax({
   		dataType: "json",
