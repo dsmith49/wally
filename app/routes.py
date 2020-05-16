04d74c871cd8b5928c9af6df9b5ec05af67741a2
david <dsmith49@gmail.com>
2020-05-16 11:05:04 -0500

    none

--- app/routes.py
+++ app/routes.py
@@ -10,5 +10,5 @@ def index():
 def command():
 	content = request.json
 	print(content['command'])
-	print(app.config['wally'].command( content )
+	app.config['wally'].command( content )
 	return ('', 204)
