b419a6eadd1f029b3a92f14a37db09f0fca5481f
david <dsmith49@gmail.com>
2020-05-16 10:16:58 -0500

    none

--- app/static/app.js
+++ app/static/app.js
@@ -16,11 +16,24 @@ document.body.appendChild(stats);
 document.addEventListener('keydown', function(e) {
 	if (document.activeElement === document.body ) {	
 		var code = e.which || e.keyCode;
-		if (([38,40,37,39]).includes(code)) { console.log('key pressed') }
-		if (([32]).includes(code)) { console.log('key pressed') }
-		if (([16]).includes(code)) { console.log('key pressed') }
-		if (([18]).includes(code)) { console.log('key pressed') }
-		if (([82]).includes(code)) { console.log('key pressed') }
-		if (([27]).includes(code)) { console.log('key pressed') }
+		var command = null
+		if (([38,87]).includes(code)) { command = 'UP' }
+		if (([88,40]).includes(code)) { command = 'DOWN' }
+		if (([65,37]).includes(code)) { command = 'LEFT' }
+		if (([39,68]).includes(code)) { command = 'RIGHT' }
+		if (([81]).includes(code)) { command = 'UPLEFT' }
+		if (([69]).includes(code)) { command = 'UPRIGHT' }
+		if (([90]).includes(code)) { command = 'DOWNLEFT' }
+		if (([67]).includes(code)) { command = 'DOWNRIGHT' }
+		if (([83,32]).includes(code)) { command = 'STOP' }
+		if (([221]).includes(code)) { command = 'PENUP' }
+		if (([219]).includes(code)) { command = 'PENDOWN' }
+		if (([49]).includes(code)) { command = 'PEN1' }
+		if (([50]).includes(code)) { command = 'PEN2' }
+		if (([51]).includes(code)) { command = 'PEN3' }
+		if (([192]).includes(code)) { command = 'POWER' }
+		if (([80]).includes(code)) { command = 'CALIBRATE' }
+		if (([27]).includes(code)) { command = 'END' }
+		console.log(command)
 	}
 });
