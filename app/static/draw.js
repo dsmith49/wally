d090b5033a11a51c1f054539aa3befc7b3b81e54
david <dsmith49@gmail.com>
2020-05-16 15:44:35 -0500

    none

--- app/static/draw.js
+++ app/static/draw.js
@@ -13,7 +13,7 @@ function loadsvg() {
 	var filename = select.options[select.selectedIndex].value
 	var astring = "{{url_for('static', filename='images/" + filename + "')}}"
 	img.src = '"' + astring + '"'
-	console.log( document.getElementById("svg_container").childNodes[0].src )
+	console.log( document.getElementById("svg_container").childNodes[0] )
 	document.getElementById("svg_container").appendChild( img )
 
 
