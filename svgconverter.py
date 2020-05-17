8366cc8cae83367f6491623b4cf3ecf94ce68b32
david <dsmith49@gmail.com>
2020-05-16 19:30:04 -0500

    none

---
+++ svgconverter.py
@@ -0,0 +1,137 @@
+import linedraw
+import sys
+from svgpathtools import svg2paths, wsvg, Path, Line
+
+import sys
+import os
+from PIL import Image
+from math import ceil
+import config
+
+#xmin=0
+#xmax=0
+#ymin=0
+#ymax=0
+#paths, attributes = svg2paths(sys.argv[1])
+#for path in paths:
+#	for line in path:
+#		print(line)
+#		xmin = min(line[0].real,line[1].real,xmin)
+#		ymin = min(line[0].imag,line[1].imag,ymin)
+#		xmax = max(line[0].real,line[1].real,xmax)
+#		ymax = max(line[0].imag,line[1].imag,ymax)
+
+#print('x max and min', xmax, xmin)
+#print('y max and min',ymax,ymin)
+#print('width =',xmax + abs(xmin))
+#print('height = ',ymax + abs(ymin))
+
+#newpaths = []
+#for path in paths:
+#	newpath = Path()
+#	for line in path:
+#		newline = Line(complex(line[0].real + abs(xmin), line[0].imag + abs(ymin)), complex(line[1].real + abs(xmin), line[1].imag + abs(ymin)))
+#		newpath.append(newline)
+#	newpaths.append(newpath)
+
+#wsvg(newpaths, filename='output.svg')
+
+class DrawObject(object):
+	def __init__(self, filename='test', imagetype='PNG', width=0,height=0,pixels=[], paths=[], attributes={}):
+		self.filename   = filename
+		self.imagetype  = imagetype
+		self.width      = width
+		self.height     = height
+		self.pixels     = pixels
+		self.paths      = paths
+		self.attributes = attributes
+
+def svgbox( pixelid, crosshatch, value, numlines ):
+	lines     		 = int( (255-value)/(256/numlines) )
+	vertical_lines   = 0
+	horizontal_lines = lines
+	if (crosshatch):
+		horizontal_lines = ceil( lines / 2)
+		vertical_lines   = lines - horizontal_lines
+	paths         = []
+	move          = []
+	origin        = [pixelid[0]*config.boxsize[0], pixelid[1]*config.boxsize[1] ]
+	current_loc   = origin.copy()
+	for y in range(0,horizontal_lines):
+		if (y == 0):
+			move = [0,(config.boxsize[1]/horizontal_lines)/2]
+			current_loc = [ current_loc[0] + move[0], current_loc[1] + move[1] ]
+		else:
+			move = [0,(config.boxsize[1]/horizontal_lines)]
+			current_loc = [ current_loc[0] + move[0], current_loc[1] + move[1] ]
+		if (y % 2 == 0):
+			move = [config.boxsize[0],0]
+		else:
+			move = [-config.boxsize[0],0]
+		paths.append( Path( Line( complex(current_loc[0], current_loc[1]), complex(current_loc[0] + move[0], current_loc[1] + move[1] ) ) ) )
+		current_loc = [ current_loc[0] + move[0], current_loc[1] + move[1] ]
+	current_loc = origin.copy()
+	if (crosshatch):
+		for y in range(0,vertical_lines):
+			if (y == 0):
+				move = [(config.boxsize[0]/vertical_lines)/2,0]
+				current_loc = [ current_loc[0] + move[0], current_loc[1] + move[1] ]
+			else:
+				move = [config.boxsize[0]/vertical_lines,0]
+				current_loc = [ current_loc[0] + move[0], current_loc[1] + move[1] ]
+			if (y % 2 == 0):
+				move = [0,config.boxsize[1]]
+			else:
+				move = [0,-config.boxsize[1]]
+			paths.append( Path( Line( complex(current_loc[0], current_loc[1]), complex(current_loc[0] + move[0], current_loc[1] + move[1] ) ) ) )
+			current_loc = [ current_loc[0] + move[0], current_loc[1] + move[1] ]
+		current_loc = origin.copy()			
+	return paths
+
+def loadfile( filename ):
+	drawobject = None
+	if (filename == 'test'):
+		if ( len( sys.argv ) > 3): config.numlines = int( sys.argv[3] )
+		width  = ceil( numlines**0.5 )
+		height = ceil( numlines**0.5 )
+		pixels = [[-1*(x*(256/numlines) - 255),255] for x in range(0, int( ceil( numlines**0.5 )**2) )]
+		drawobject = DrawObject( imagetype='PNG', height=height, width=width,pixels=pixels)
+	elif (filename.split('.')[1] == 'png'):
+		print('loading file: ',filename )
+		img     = Image.open( filename )
+		width   = img.size[0]
+		height  = img.size[1]
+		pix_val = list(img.getdata())
+		pixels = [[x,a] for (x,_,_,a) in pix_val]
+		drawobject = DrawObject( filename=filename.split('.')[0], imagetype='PNG', height=height, width=width,pixels=pixels)
+	else:
+		print('file must be PNG')
+		exit()
+	return drawobject
+
+def pngtosvg( data, filename, crosshatch, numlines ):
+	newpaths = []
+	for y in range(0,data.height):
+		for x in range(0,data.width):
+			if (data.pixels[data.width*y + x][1] != 0):
+				newpaths = newpaths + svgbox( [x,y], crosshatch, data.pixels[data.width*y + x][0], numlines)
+	wsvg( newpaths, filename=filename.split('.')[0] + '.svg')
+
+def main():
+	if ( len(sys.argv) not in [5] ):
+		print('USAGE: python3 svgconverter.py <filename> <CONTOUR|PIXEL> <hatching INT> <contour INT>')
+		print('if mode is CONTOUR, hatching in [0,32], contour in [0,4]')
+		print('if mode is PIXEL, hatching > 0 sets crosshatch to True, else False, contour is numlines per pixel, in [0,32]')
+		exit()
+	filename     = sys.argv[1]
+	convert_type = sys.argv[2]
+	hatching     = int(sys.argv[3])
+	contour      = int(sys.argv[4])
+	print( os.path.split(filename) )
+	if (convert_type == 'CONTOUR'):
+		linedraw.vectorise(filename.split('.')[0], draw_hatch=hatching,draw_contours=contour)
+	if (convert_type == 'PIXEL'):
+		data  = loadfile( filename )
+		pngtosvg( data, filename, hatching, contour)
+
+main()
---
+++ wally_controller.py
@@ -0,0 +1,166 @@
+import json
+import time
+import motorlib
+import config
+from svgpathtools import svg2paths, Path, Line
+
+class DrawObject(object):
+	def __init__(self, filename='test', imagetype='PNG', width=0,height=0,pixels=[], paths=[], attributes={}):
+		self.filename   = filename
+		self.imagetype  = imagetype
+		self.width      = width
+		self.height     = height
+		self.pixels     = pixels
+		self.paths      = paths
+		self.attributes = attributes
+
+class Wally(object):
+	def __init__(self):
+		self.motors_on = False
+		self.ever_on   = False
+		self.motors_velocity = [0,0]
+		self.motors_position = [int(config.motor1_length / config.meters_per_step), int(config.motor2_length / config.meters_per_step) ]
+		self.motors_last_velocity = [0,0]
+		self.timestamp_1 = time.perf_counter()
+		self.increment = 500
+		self.pwm = None #motorlib.configmotors( 0 )
+		self.pendown = False
+
+	def status(self):
+		statusdict = {
+			'power'    : self.motors_on,
+			'pendown'  : self.pendown,
+			'velocity' : self.motors_velocity,
+			'position_steps'    : self.motors_position,
+			'position_meters'   : [self.motors_position[0] * config.meters_per_step, self.motors_position[1] * config.meters_per_step],
+			'position_euclid'   : motorlib.hypoteni_to_euclid( self.motors_position )
+		}
+		return statusdict #json.dumps( statusdict )
+
+	def power(self, on=True, final=False):
+		if (on and not self.motors_on):
+			self.pwm = motorlib.configmotors( 0, first = not self.ever_on )
+			self.motors_on = True
+			self.ever_on   = True
+		if (not on and self.motors_on):
+			motorlib.close( self.pwm )
+			self.motors_on = False
+		if (final):
+			motorlib.close( self.pwm, final=True)
+
+	def pause(self):
+		motorlib.stop()
+
+	def pen_move(self, down=None):
+		if (down is None):
+			self.pendown = not self.pendown
+		else:
+			self.pendown = down
+		if (not self.pendown):
+			motorlib.setangle(self.pwm,0,config.pen_up_angle)
+		else:
+			motorlib.setangle(self.pwm,0,config.pen_down_angle)
+
+	def pen_rotate(self, position):
+		self.pen_move( down=False )
+		if (position==0): motorlib.setangle(self.pwm,1,config.pen_1_angle)
+		if (position==1): motorlib.setangle(self.pwm,1,config.pen_2_angle)
+		if (position==2): motorlib.setangle(self.pwm,1,config.pen_3_angle)
+
+	def calibrate(self):
+		self.motors_velocity = [0,0]
+		self.motors_position = [int(config.motor1_length / config.meters_per_step), int(config.motor2_length / config.meters_per_step) ]
+
+	def drawSVG( self, data ):
+		position = [0.0,0.0]
+		for num,path in enumerate(data.paths):
+			print(path, num,'of',len(data.paths))
+			x = path[0][0].real*config.meters_per_step*config.svg_pixel_size - position[0]
+			y = path[0][0].imag*config.meters_per_step*config.svg_pixel_size - position[1]
+			print('move', [x,y],'from',position, 'and drop pen')
+			self.motors_position = motorlib.move( config.speed, [x,y], self.motors_position )
+			position[0] += x
+			position[1] += y
+			self.pen_move()
+			print(path, num,'of',len(data.paths))
+			for num2,line in enumerate(path):
+				x = line[1].real*config.meters_per_step*config.svg_pixel_size - position[0]
+				y = line[1].imag*config.meters_per_step*config.svg_pixel_size - position[1]
+				print('move', [x,y],'from',position)
+				self.motors_position = motorlib.move( config.speed, [x,y], self.motors_position )
+				position[0] += x
+				position[1] += y
+			self.pen_move()
+			print('done path gondola is at', position)
+
+	def loadfile(self, filename ):
+		drawobject = None
+		if (filename == 'test'):
+			if ( len( sys.argv ) > 2): config.numlines = int( sys.argv[2] )
+			width  = math.ceil( numlines**0.5 )
+			height = math.ceil( numlines**0.5 )
+			pixels = [[-1*(x*(256/numlines) - 255),255] for x in range(0, int( math.ceil( numlines**0.5 )**2) )]
+			drawobject = DrawObject( imagetype='PNG', height=height, width=width,pixels=pixels)
+		elif (filename.split('.')[1] == 'png'):
+			print('loading file: ',filename )
+			img     = Image.open( filename )
+			width   = img.size[0]
+			height  = img.size[1]
+			pix_val = list(img.getdata())
+			pixels = [[x,a] for (x,_,_,a) in pix_val]
+			drawobject = DrawObject( filename=filename.split('.')[0], imagetype='PNG', height=height, width=width,pixels=pixels)
+		elif (filename.split('.')[1] == 'svg'):
+			print('loading file: ',filename )
+			paths, attributes = svg2paths(filename)
+			drawobject = DrawObject( imagetype='SVG', paths=paths, attributes=attributes)
+		return drawobject
+
+	def command(self, command_json):
+		self.motors_last_velocity = self.motors_velocity.copy()
+		command = command_json['command']
+		print('command:',command)
+		if (not self.motors_on and command != 'POWER'): return None
+		if (command == 'UP'):
+			self.motors_velocity[0] -= self.increment
+			self.motors_velocity[1] -= self.increment
+		elif (command == 'DOWN'):
+			self.motors_velocity[0] += self.increment
+			self.motors_velocity[1] += self.increment
+		elif (command == "STOP"):
+			self.motors_velocity[0] = 0
+			self.motors_velocity[1] = 0
+		elif (command == "UPLEFT"):
+			self.motors_velocity[0] -= self.increment
+		elif (command == "UPRIGHT"):
+			self.motors_velocity[1] -= self.increment
+		elif (command == "DOWNLEFT"):
+			self.motors_velocity[1] += self.increment
+		elif (command == "DOWNRIGHT"):
+			self.motors_velocity[0] += self.increment
+		elif (command == "LEFT"):
+			self.motors_velocity[0] -= self.increment
+			self.motors_velocity[1] += self.increment
+		elif (command == "RIGHT"):
+			self.motors_velocity[0] += self.increment
+			self.motors_velocity[1] -= self.increment
+		elif (command == "PEN1"): self.pen_rotate(0)
+		elif (command == "PEN2"): self.pen_rotate(1)
+		elif (command == "PEN3"): self.pen_rotate(2)
+		elif (command == "PENDOWN"): self.pen_move(down=True)
+		elif (command == "PENUP"): self.pen_move(down=False)
+		elif (command == "POWER"): self.power(on=not self.motors_on)
+		elif (command == "END"): self.power(on=False, final=True)
+		elif (command == "CALIBRATE"): self.calibrate()
+		elif (command in ["MOVE","DRAW"]):
+			self.motors_velocity = [0,0]
+		if (self.motors_on):
+			self.motors_position = motorlib.update_motors( self.motors_last_velocity, self.motors_velocity, self.timestamp_1, self.motors_position)
+			self.timestamp_1 = time.perf_counter()
+		if (command == "MOVE"):
+			self.motors_position = motorlib.move_smart2( command_json['speed'], command_json['relative_coords'], self.motors_position )
+		if (command == "DRAW"):
+			print('loading', command_json['filename'] )
+			data = self.loadfile( command_json['filename'] )
+			self.drawSVG( data )
+		time.sleep(config.button_delay)
+
