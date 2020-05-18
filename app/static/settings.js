e58d403cc67602193991fb3eb94ebcd399c273ca
david <dsmith49@gmail.com>
2020-05-17 19:56:34 -0500

    none

--- app/static/settings.js
+++ app/static/settings.js
@@ -11,7 +11,9 @@ function getsettings() {
 
 function updatestatus( data ) {
 	Object.keys(data).map( key => {
+		console.log('checking for', key)
 		if (document.getElementById(key) != null) {
+			console.log('setting',key,data[key])
 			document.getElementById(key).value = data[key]
 		}
 	})
